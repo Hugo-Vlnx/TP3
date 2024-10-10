01cc9ed496792b0eb9151cfeb121c478938be259
Hugo.V <hugo.vialaneix@gmail.com>
2024-10-10 16:57:17 +0200

    activite +objet

--- projectfun/maint.c
+++ projectfun/maint.c
@@ -1,9 +1,74 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-int main()
+
+void activite()
 {
-	printf("i love git");
-	printf("Omg it works");
+	char activite_A[100];
+	char activite_B[100];
+	char activite_C[100];
 
-	return  0;
+	printf("entrez votre activité n1 : ");
+	fgets(activite_A, sizeof(activite_A), stdin);
+
+	printf("entrez votre activité n-2 : ");
+	fgets(activite_B, sizeof(activite_B), stdin);
+
+	printf("entrez votre activité n-3 : ");
+	fgets(activite_B, sizeof(activite_B), stdin);
+
+	srand(time(NULL));
+
+	int random = rand() % 3;
+
+	printf("Activite choisie aleatoirement : ");
+	if (random == 0) {
+		printf("%s", activite_A);
+	}
+	else if (random == 1) {
+		printf("%s", activite_B);
+	}
+	else {
+		printf("%s", activite_C);
+	}
+
+
+	return 0;
+
+}
+int main()
+void objet()
+{
+	char objet_A[100];
+	char objet_B[100];
+	char objet_C[100];
+
+	printf("entrez votre activité n°1 : ");
+	fgets(objet_A, sizeof(objet_A), stdin);
+
+	printf("entrez votre activité n°1 : ");
+	fgets(objet_B, sizeof(objet_B), stdin);
+
+	printf("entrez votre activité n°1 : ");
+	fgets(objet_B, sizeof(objet_B), stdin);
+
+	srand(time(NULL));
+
+	int random = rand() % 3;
+
+	printf("Activite choisie aleatoirement : ");
+	if (random == 0) {
+		printf("%s", objet_A);
+	}
+	else if (random == 1) {
+		printf("%s", objet_B);
+	}
+	else {
+		printf("%s", objet_C);
+	}
+
+
+	return 0;
+
 }
