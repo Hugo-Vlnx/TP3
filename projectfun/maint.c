2373aef58d98d8db35fa1994de5ccfa74103642e
Hugo.V <hugo.vialaneix@gmail.com>
2024-10-10 17:38:43 +0200

    new

--- projectfun/maint.c
+++ projectfun/maint.c
@@ -3,42 +3,7 @@
 #include <stdlib.h>
 
 
-char activite()
-{
-	char activite_A[100];
-	char activite_B[100];
-	char activite_C[100];
-
-	printf("entrez votre activité n1 : ");
-	fgets(activite_A, sizeof(activite_A), stdin);
-
-	printf("entrez votre activité n-2 : ");
-	fgets(activite_B, sizeof(activite_B), stdin);
-
-	printf("entrez votre activité n-3 : ");
-	fgets(activite_B, sizeof(activite_B), stdin);
-
-	srand(time(NULL));
-
-	int random = rand() % 3;
-
-	printf("Activite choisie aleatoirement : ");
-	if (random == 0) {
-		printf("%s", activite_A);
-	}
-	else if (random == 1) {
-		printf("%s", activite_B);
-	}
-	else {
-		printf("%s", activite_C);
-	}
-
-
-	return 0;
-
-}
-
-char objet()
+int main()
 {
 	char objet_A[100];
 	char objet_B[100];
@@ -59,19 +24,55 @@ char objet()
 
 	printf("Activite choisie aleatoirement : ");
 	if (random == 0) {
-		return objet_A;
+		printf("%s", objet_A);
 	}
 	else if (random == 1) {
-		return objet_B;
+		printf("%s", objet_B);
 	}
 	else {
-		return objet_C;
+		printf("%s", objet_C);
 	}
+
+
+	return 0;
+
 }
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
 
 int main()
 {
-	char activite[100] = activite();
-	char objet[100] = objet();
-	printf("%s et %s", activite, objet);
+	char activite_A[100];
+	char activite_B[100];
+	char activite_C[100];
+
+	printf("entrez votre activité n°1 : ");
+	fgets(activite_A, sizeof(activite_A), stdin);
+
+	printf("entrez votre activité n°1 : ");
+	fgets(activite_B, sizeof(activite_B), stdin);
+
+	printf("entrez votre activité n°1 : ");
+	fgets(activite_B, sizeof(activite_B), stdin);
+
+	srand(time(NULL));
+
+	int random = rand() % 3;
+
+	printf("Activite choisie aleatoirement : ");
+	if (random == 0) {
+		printf("%s", activite_A);
+	}
+	else if (random == 1) {
+		printf("%s", activite_B);
+	}
+	else {
+		printf("%s", activite_C);
+	}
+
+
+	return 0;
+
 }
