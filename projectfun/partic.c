7c860fc6cb6c2fb1d77ae733b5925cc1c5ef17a3
Hugo.V <hugo.vialaneix@gmail.com>
2024-10-10 16:49:26 +0200

    test

---
+++ projectfun/partic.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	char activite_A[100];
+	char activite_B[100];
+	char activite_C[100];
+
+	printf("entrez votre activité n°1 : ");
+	fgets(activite_A, sizeof(activite_A), stdin);
+
+	printf("entrez votre activité n°1 : ");
+	fgets(activite_B, sizeof(activite_B), stdin);
+
+	printf("entrez votre activité n°1 : ");
+	fgets(activite_B, sizeof(activite_B), stdin);
+
+	srand(time(NULL));
+
+	int random = rand() % 3;
+
+	printf("Activite choisie aleatoirement : ");
+	if (random == 0) {
+		printf("%s", activite_A);
+	}
+	else if (random == 1) {
+		printf("%s", activite_B);
+	}
+	else {
+		printf("%s", activite_C);
+	}
+
+
+	return 0;
+
+}
